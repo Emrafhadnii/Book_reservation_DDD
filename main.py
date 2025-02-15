82475fc91dbfce506d48f8d758ff58c34eeaaa16
Emrafhadnii <emrafhadnii.rad@gmail.com>
2025-02-16 00:20:57 +0330

    fixing some bugs again!

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
 from src.entrypoints.getbook import router as book_router
 from src.entrypoints.auth import router
+from src.entrypoints.reservation import router as reserve_route
+
 
 app = FastAPI()
 
 app.include_router(book_router)
 app.include_router(router)
-
+app.include_router(reserve_route)
 
 
 
