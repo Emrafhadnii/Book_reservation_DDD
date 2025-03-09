95bade51b86800bb718843555ce60583888a2a19
Emrafhadnii <emrafhadnii.rad@gmail.com>
2025-03-09 16:37:53 +0330

    testing cache place for read queries

--- src/books/entrypoints/book_router.py
+++ src/books/entrypoints/book_router.py
@@ -3,6 +3,10 @@ from src.auth.entrypoints.dependencies.userauth import get_uow,get_current_user
 from src.adapters.UOW import UnitOfWork
 from src.books.domain.entities.Books import Book
 from src.books.adapters.mongo_repositories.Mongo_BookRepo import MongoDBBookRepository
+from src.auth.entrypoints.dependencies.otp_dependency import get_redis
+from redis import Redis
+import json
+
 
 router = APIRouter(prefix='/book',tags=['book'])
 
@@ -18,9 +22,20 @@ async def get_books(repos: UnitOfWork = Depends(get_uow), token = Depends(get_cu
     return list(Book.model_validate(book) for book in books)
 
 @router.get('/{book_id}')
-async def get_book(book_id, repos: UnitOfWork = Depends(get_uow), token = Depends(get_current_user)):
+async def get_book(book_id: int, repos: UnitOfWork = Depends(get_uow), token = Depends(get_current_user),
+                   redis: Redis = Depends(get_redis)):
+    
+    cache_key = f"book_{int(book_id)}"
+    book_cached = await redis.get(cache_key)
+    
+    if book_cached:
+        return json.loads(book_cached)
+    
     book_repo = repos.book
     book = await book_repo.get_by_id(book_id)
+    
+    await redis.setex(cache_key,60,json.dumps(dict(book)).encode('utf-8'))
+    
     return dict(book)
 
 @router.get('/search/{text}')
